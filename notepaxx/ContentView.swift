d804c8d224e1d6a97c36281beb1f24d4399e8ec1
Brian Angulo <bangulo219@gmail.com>
2022-08-13 17:39:15 -0500

    Initial Commit

---
+++ notepaxx/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notepaxx
+//
+//  Created by Brian Angulo on 8/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notepaxx/notepaxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  notepaxxApp.swift
+//  notepaxx
+//
+//  Created by Brian Angulo on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct notepaxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
